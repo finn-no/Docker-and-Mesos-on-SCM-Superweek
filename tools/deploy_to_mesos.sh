f9122412b5daeaf84c7a3e40b9bce73c777db961
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-04-22 11:22:26 +0200

    Use correct directories for finding marathon.json

--- tools/deploy_to_mesos.sh
+++ tools/deploy_to_mesos.sh
@@ -2,7 +2,8 @@
 
 TAG=$1
 MARATHON_FILE=$2
-pwd
-cat ./$MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
+PRES_DIR=${HOME}/${CIRCLE_PROJECT_REPONAME}/presentation
+
+cat ${PRES_DIR}/$MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
 
 curl -i -F filedata=@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080
