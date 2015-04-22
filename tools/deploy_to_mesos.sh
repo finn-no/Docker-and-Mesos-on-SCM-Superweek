5dfc68573bdb07eed5ce61e386994bb207a30b90
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-04-22 11:15:43 +0200

    Store the created marathon.json in artifacts dir

--- tools/deploy_to_mesos.sh
+++ tools/deploy_to_mesos.sh
@@ -2,7 +2,7 @@
 
 TAG=$1
 MARATHON_FILE=$2
+pwd
+cat ./$MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
 
-cat $MARATHON_FILE | envsubst | tee /tmp/marathon.json
-
-curl -i -F filedata=@/tmp/marathon.json http://mesos.nisgits.net:8080
+curl -i -F filedata=@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080
