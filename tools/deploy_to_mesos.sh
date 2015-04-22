5c24e3935dd9eac4b61497c69b1a4c8b0ced5ec3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-04-22 11:28:01 +0200

    Fix usage of Marathon API for updating app

--- tools/deploy_to_mesos.sh
+++ tools/deploy_to_mesos.sh
@@ -6,4 +6,4 @@ PRES_DIR=${HOME}/${CIRCLE_PROJECT_REPONAME}/presentation
 
 cat ${PRES_DIR}/$MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
 
-curl -i -F filedata=@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080
+curl -i -F filedata=@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080/v2/apps/docker-and-mesos-presentation
