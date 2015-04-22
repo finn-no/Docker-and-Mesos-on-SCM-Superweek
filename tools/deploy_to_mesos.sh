b44a7f6ac9d34048a7dadee65b4db7c324442f8a
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-04-22 12:07:56 +0200

    No need for complex dir logic with correct name

--- tools/deploy_to_mesos.sh
+++ tools/deploy_to_mesos.sh
@@ -2,8 +2,7 @@
 
 TAG=$1
 MARATHON_FILE=$2
-PRES_DIR=${HOME}/${CIRCLE_PROJECT_REPONAME}/presentation
 
-cat ${PRES_DIR}/$MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
+cat $MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
 
 curl -i -F filedata=@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080/v2/apps/docker-and-mesos-presentation
