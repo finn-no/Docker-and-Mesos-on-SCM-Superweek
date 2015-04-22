11ad1b4359141a89ed09f85a5e804d00ecfbfaa0
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-04-22 13:39:49 +0200

    Fix path of uploaded marathon.json

--- tools/deploy_to_mesos.sh
+++ tools/deploy_to_mesos.sh
@@ -5,4 +5,4 @@ MARATHON_FILE=$2
 
 cat $MARATHON_FILE | envsubst | tee ${CIRCLE_ARTIFACTS}/marathon.json
 
-curl -L -i -X PUT -H "Content-Type: application/json" -d@marathon.json http://mesos.nisgits.net:8080/v2/apps/docker-and-mesos-presentation
+curl -L -i -X PUT -H "Content-Type: application/json" -d@${CIRCLE_ARTIFACTS}/marathon.json http://mesos.nisgits.net:8080/v2/apps/docker-and-mesos-presentation
